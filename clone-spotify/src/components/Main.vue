7ebdb829875649b87a37a37b3db3322e87dfa120
Yan0606 <yan060604@gmail.com>
2023-10-01 01:45:25 -0300

    adicionando o primeiro card

--- clone-spotify/src/components/Main.vue
+++ clone-spotify/src/components/Main.vue
@@ -1,17 +1,28 @@
 <template>
-   <div class="flex h-full">
-    <div class="bg-gray-900 p-4 rounded-lg shadow-md w-full mb-1 flex justify-between ">
-        <div class="flex ">
-            <img src="../components/img/seta-esquerda.png" alt="seta para esquerda" class="w-15 h-5 mr-2">
-            <img src="../components/img/seta-direita.png" alt="seta para direita" class="w-15 h-5">
+    <div class="flex h-full flex-col">
+        <div class="bg-gray-900 p-4 rounded-lg shadow-md w-full mb-1 flex justify-between">
+            <div class="flex p-1.5">
+                <img src="../components/img/seta-esquerda.png" alt="seta para esquerda" class="w-15 h-5 mr-2">
+                <img src="../components/img/seta-direita.png" alt="seta para direita" class="w-15 h-5">
+            </div>
+            <div class="flex">
+                <a href="#">
+                    <p class="ml-4 text-gray-300 visited:text-slate-50 p-1.5">Inscrever-se</p>
+                </a>
+                <button class="bg-white text-black rounded-full h-10 w-36 ml-4">Entrar</button>
+            </div>
         </div>
-        <div class="flex ">
-            <a href="#"><p class="ml-4 text-gray-300 visited:text-slate-50">Inscrever-se</p></a>
-            <button class="bg-white text-black rounded-full h-10 w-36 ml-4">Entrar</button>
+        <div class="bg-gray-800 p-4 rounded-lg shadow-md w-74 mb-3">
+            <h1 class="text-xl mt-4 text-white">Playlists do Spotify</h1>
+            <div class="bg-gray-900 max-w-xs rounded overflow-hidden shadow-lg">
+                <img class="w-5/6 " src="../components/img/tue.jpg" alt="Imagem do matuê">
+                <div class="px-6 py-4">
+                    <p class="text-xl font-semibold mb-2">Matuê</p>
+                    <p class="text-gray-700 text-base">Um dos melhores se não o melhor trapper da sena brasileira hoje</p>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
 </template>
   
 <script>
