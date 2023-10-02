62628f291199069262028213d5cb11f1150af6ac
Yan0606 <yan060604@gmail.com>
2023-10-01 22:32:24 -0300

    atualizando a Main, colocando todos os cards

--- clone-spotify/src/components/Main.vue
+++ clone-spotify/src/components/Main.vue
@@ -12,13 +12,59 @@
                 <button class="bg-white text-black rounded-full h-10 w-36 ml-4">Entrar</button>
             </div>
         </div>
-        <div class="bg-gray-800 p-4 rounded-lg shadow-md w-74 mb-3">
-            <h1 class="text-xl mt-4 text-white">Playlists do Spotify</h1>
-            <div class="bg-gray-900 max-w-xs rounded overflow-hidden shadow-lg">
-                <img class="w-5/6 " src="../components/img/tue.jpg" alt="Imagem do matuê">
-                <div class="px-6 py-4">
-                    <p class="text-xl font-semibold mb-2">Matuê</p>
-                    <p class="text-gray-700 text-base">Um dos melhores se não o melhor trapper da sena brasileira hoje</p>
+        <div class="bg-gray-800 p-4 rounded-xl shadow-md w-74 mb-3">
+            <h1 class="text-xl mt-4 text-white py-2">Playlists do Spotify</h1>
+            <div class="flex space-x-3">
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/thebest.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">This is Michael Jackson</p>
+                        <p class="text-gray-400 text-base">A melhor artista Pop de todas as eras
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/tue.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">This is Matuê</p>
+                        <p class="text-gray-400 text-base">Um dos melhores se não o melhor trapper da cena brasileira hoje
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/taylor.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">This is Taylor Swift</p>
+                        <p class="text-gray-400 text-base">A melhor artista do ano de 2023
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/rhcp.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">This is Red Hot Chilli Peppers</p>
+                        <p class="text-gray-400 text-base">A banda de Rock com 6 Grammy
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <h1 class="text-xl mt-4 text-white py-2">Relax de fim de Domingo</h1>
+            <div class="flex space-x-3">
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/top.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">Top Brasil</p>
+                        <p class="text-gray-400 text-base">Veigh, Ana Castela, Mc Ryan, Henrique & Juli...
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-gray-900 w-52 rounded-xl overflow-hidden shadow-lg mr-2">
+                    <img class="w-48 h-44 px-2 py-2 rounded-xl ml-2" src="../components/img/hits.jpg" alt="Imagem do matuê">
+                    <div class="px-2 py-4">
+                        <p class="text-xl font-semibold mb-2 text-white">Hits da internet</p>
+                        <p class="text-gray-400 text-base">Matuê, MC Ryan SP, Ana Castela, Luísa Sonza,Fel...
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
